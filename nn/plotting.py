3a4d7ce7e5928e458d0daf1199e19956c3bda081
Benjamin <benjamincf01@gmail.com>
2025-07-25 00:21:35 +0200

    show wrong predictions function

--- nn/plotting.py
+++ nn/plotting.py
@@ -1,16 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_pred_grid(net, X_test, Y_test, grid_size:int=8, 
+def plot_pred_grid(net, X_test, Y_test, shape:tuple[int, int]=[8, 8], 
                    only_wrong=False):
 
-    Y_test_label = Y_test
-    fig, axes = plt.subplots(8,8, figsize=(8,8))
-    fig.tight_layout(pad=0.1)
+    fig, axes = plt.subplots(shape[0],shape[1], figsize=(shape[1],shape[0]))
+    fig.tight_layout()
 
-    for ax in axes.flat:
+    y_pred = np.argmax(net.predict(X_test), axis=1)
+    y_test = np.argmax(Y_test, axis=1)
+
+    print(y_pred.shape, y_test.shape)
+
+    if only_wrong:
+        poss_indices = np.where(y_pred != y_test)[0]
+        print(poss_indices.shape)
+    else:
+        poss_indices = np.arange(y_test.shape[0])
+
+    random_sample = np.random.choice(poss_indices, size=shape[0]*shape[1], replace=False)
+
+    for i, ax in enumerate(axes.flat):
         # Select random indices
-        random_index = np.random.randint(10000)
+        random_index = random_sample[i]
         
         # Select rows corresponding to the random indices and
         # reshape the image
@@ -19,10 +31,9 @@ def plot_pred_grid(net, X_test, Y_test, grid_size:int=8,
         # Display the image
 
         ax.imshow(X_random_reshaped, cmap='gray')
-        y_pred = net.predict(X_random_reshaped.reshape((1, 784)))
-        y_pred_label = np.argmax(y_pred, axis=1)
-        
+        y_pred_val = y_pred[random_index]
+        y_test_val = y_test[random_index]
         
         # Display the label above the image
-        ax.set_title(f'{Y_test_label[random_index]} , {y_pred_label[0]}')
+        ax.set_title(f'L:{y_test_val} P:{y_pred_val}')
         ax.set_axis_off()
